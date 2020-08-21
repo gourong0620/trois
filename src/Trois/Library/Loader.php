99f419f31dac3efa9d2fab1570469feb73cc231f
“luren <1012004024@qq.com>
2020-08-21 14:40:49 +0800

    commit something

---
+++ src/Trois/Library/Loader.php
@@ -0,0 +1,18 @@
+<?php
+/*
+ * @Descripttion: 
+ * @version: 
+ * @Author: luren
+ * @Date: 2020-08-21 14:37:00
+ * @LastEditors: luren
+ */
+
+namespace Trois\Library;
+
+class Loader
+{
+    public function index()
+    {
+        echo 'hello Loader';
+    }
+}
